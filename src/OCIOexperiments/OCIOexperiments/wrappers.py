0dd915b2521de5b01672d9d9f2fde2fec92e1a93
Liam Collod <lcollod@gmail.com>
2022-05-14 19:17:34 +0200

    ocex: wrappers: fixed issue compared to the OCIO implementation
    OCIO apply differently the master on the RGB values depending on the operations.

--- src/OCIOexperiments/OCIOexperiments/wrappers.py
+++ src/OCIOexperiments/OCIOexperiments/wrappers.py
@@ -1,7 +1,7 @@
 """
 
 """
-from typing import Union, Tuple
+from typing import Union, Tuple, Literal
 
 import PyOpenColorIO as ocio
 
@@ -10,27 +10,35 @@ __all__ = ("to_rgbm",)
 
 def to_rgbm(
     value: Union[float, Tuple[float, float, float]],
+    ocio_math: Literal["*", "+"] = "*",
 ) -> ocio.GradingRGBM:
-    """
+    r"""
     Convert a python object to an OCIO GradingRGBM instance to use for GradingPrimary.
 
     Args:
+        ocio_math:
+            how OCIO internally applies the master on the R-G-B components, check:
+            `src\OpenColorIO\ops\gradingprimary\GradingPrimary.cpp`
         value: a value to apply on all channel or one different per-channel (RGB)
 
     Returns:
         GradingRGBM instance
     """
+    if ocio_math == "*":
+        x = 1
+    else:
+        x = 0
 
     grgbm = ocio.GradingRGBM()
     if isinstance(value, float):
-        grgbm.red = 1.0
-        grgbm.green = 1.0
-        grgbm.blue = 1.0
-        grgbm.master = value
+        grgbm.red = value
+        grgbm.green = value
+        grgbm.blue = value
+        grgbm.master = x
     else:
         grgbm.red = value[0]
         grgbm.green = value[1]
         grgbm.blue = value[2]
-        grgbm.master = 1.0
+        grgbm.master = x
 
     return grgbm
